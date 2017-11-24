47867ac1fd01c1a4278437d2f287ea5e53cbd5fa
lex00 <dontemailmeplease.com>
2017-11-24 16:47:17 -0700

    tested on local 14.04 docker

--- ci_ubuntu_14.04.sh
+++ ci_ubuntu_14.04.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-apt-get update -y -qq
-apt-get install python-pip
+cd /flask-uwsgi-nginx
+apt-get update -y
+
+# python setup
+apt-get install -y python-dev python-pip
+pip install --upgrade pip setuptools
+
+# for crypto tools?
+apt-get install -y libffi-dev libssl-dev
+
+# install ansible
 pip install ansible ansible-lint
 
 # Check the role/playbook's syntax.
