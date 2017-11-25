8da6bd1e1072363db3bb14dfe51ac7f64ff94b44
lex00 <dontemailmeplease.com>
2017-11-25 11:58:51 -0700

    set flag at runtime to detect docker.  only run systemd handlers if not is_docker. sheesh!

--- tests/ci_ubuntu.sh
+++ tests/ci_ubuntu.sh
@@ -3,21 +3,10 @@
 cd /flask-uwsgi-nginx
 apt-get update -y
 
-apt-get update
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible
-
-# python setup
-#apt-get install -y sudo python-dev python-pip
-#pip install --upgrade pip setuptools
-
-# for crypto tools?
-#apt-get install -y libffi-dev libssl-dev
-
-# install ansible
-#pip install ansible ansible-lint
+apt-get install -y sudo software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update -y
+apt-get install -y ansible
 
 # Check the role/playbook's syntax.
 ansible-playbook -i tests/inventory --extra-vars="@tests/test.json" tests/test.yml --syntax-check
